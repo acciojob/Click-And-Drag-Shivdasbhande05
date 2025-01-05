159f5b18c5f4014e9a62ff3f6f7d783d4bedea1b
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-05 23:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Your code here.
+let pElements = document.getElementByClass("items");
+let parentElement = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+parentElement.eventListener('mousedown', (e) =>{
+	isDown = true;
+	pElements.classList.add("active");
+	startX = e.pageX - pElements.offSetLeft;
+	scrollLeft = e.scrollLeft;
+});
