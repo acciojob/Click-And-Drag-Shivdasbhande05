46e7d0612750953bd490589e9bf5e50b43272fd6
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-05 23:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ parentElement.eventListener('mousedown', (e) =>{
 	startX = e.pageX - pElements.offSetLeft;
 	scrollLeft = e.scrollLeft;
 });
+
+parentElement.eventListener('mouseup', 'mouseleave' , (e) => {
+	isDown = false;
+	pElements.classList.remove("active");
+});
+
+parentElement.eventListener('mousemove', (e) =>{
+	if(!isDown) return;
+	e.preventDefault();
+	const ex = e.pageX - pElements.offSetLeft;
+	const move = (x- startX)*2;
+	pElements.scrollLeft = scrollLeft - move;
+});
