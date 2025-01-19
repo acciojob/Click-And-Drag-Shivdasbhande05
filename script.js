a48c13d9c83fd291eace86cd2a39877c304030d9
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-19 13:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,26 +5,40 @@ let scrollLeft;
 
 //When mouse down we need to track initial position
 itemsConatiner.addEventListener("mousedown",(e) => {
+    //console.log("Mousedown event triggered");
     isDown = true;
     startX = e.pageX - itemsConatiner.offsetLeft;
     scrollLeft = itemsConatiner.scrollLeft;
     itemsConatiner.classList.add("active");
+
+    setTimeout(() => {
+        itemsConatiner.dispatchEvent(new MouseEvent("mousemove" , {
+            view : window,
+            bubbles : true,
+            canelable : true,
+            clinetX : 200,
+            clientY : 100,
+        }))
+    },1000);
 });
 
 //when mouse is up we need to stop tracking
-itemsConatiner.addEventListener("mouseup" ,() => {
+itemsConatiner.addEventListener("mouseup", () => {
+    //console.log("Mouseup Event Triggered");
     isDown = false;
     itemsConatiner.classList.remove("active");
 });
 
 //when mouse leave we need to stop tracking
 itemsConatiner.addEventListener("mouseleave" , () =>{
+    //console.log("Mouleave Event Triggerd");
     isDown = false;
     itemsConatiner.classList.remove("active");  
 });
 
 // when mouse is moving we need to adjust scroll position
 itemsConatiner.addEventListener("mousemove", (e) => {
+    //console.log("Mousemove Event Triggered");
     if(!isDown){
         return;
     }
@@ -34,4 +48,12 @@ itemsConatiner.addEventListener("mousemove", (e) => {
     const x = e.pageX - itemsConatiner.offsetLeft;
     const wallk = (x-startX) * 2;// 
     itemsConatiner.scrollLeft = scrollLeft-wallk;
+
+    setTimeout(()=>{
+        itemsConatiner.dispatchEvent(new MouseEvent("mouseup",{
+            view : window,
+            bubbles : true,
+            cancelable : true
+        }));
+    },1000);
 });
